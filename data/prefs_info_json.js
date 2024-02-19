923429789f9a9adc0963f9c33e08a8ee676bb062
masato-tsuji <156637744+masato-tsuji@users.noreply.github.com>
2024-02-19 20:32:16 +0900

    Add files via upload

---
+++ data/prefs_info_json.js
@@ -0,0 +1,378 @@
+const prefsInfo = [
+    {
+        "id": "pref_01",
+        "area": "北海道",
+        "prefName": "北海道",
+        "shortName": "北海道",
+        "prefHiragana": "ほっかいどう",
+        "shortHiragana": "ほっかいどう"
+    },
+    {
+        "id": "pref_02",
+        "area": "東北",
+        "prefName": "青森県",
+        "shortName": "青森",
+        "prefHiragana": "あおもりけん",
+        "shortHiragana": "あおもり"
+    },
+    {
+        "id": "pref_03",
+        "area": "東北",
+        "prefName": "岩手県",
+        "shortName": "岩手",
+        "prefHiragana": "いわてけん",
+        "shortHiragana": "いわて"
+    },
+    {
+        "id": "pref_04",
+        "area": "東北",
+        "prefName": "宮城県",
+        "shortName": "宮城",
+        "prefHiragana": "みやぎけん",
+        "shortHiragana": "みやぎ"
+    },
+    {
+        "id": "pref_05",
+        "area": "東北",
+        "prefName": "秋田県",
+        "shortName": "秋田",
+        "prefHiragana": "あきたけん",
+        "shortHiragana": "あきた"
+    },
+    {
+        "id": "pref_06",
+        "area": "東北",
+        "prefName": "山形県",
+        "shortName": "山形",
+        "prefHiragana": "やまがたけん",
+        "shortHiragana": "やまがた"
+    },
+    {
+        "id": "pref_07",
+        "area": "東北",
+        "prefName": "福島県",
+        "shortName": "福島",
+        "prefHiragana": "ふくしまけん",
+        "shortHiragana": "ふくしま"
+    },
+    {
+        "id": "pref_08",
+        "area": "関東",
+        "prefName": "茨城県",
+        "shortName": "茨城",
+        "prefHiragana": "いばらきけん",
+        "shortHiragana": "いばらき"
+    },
+    {
+        "id": "pref_09",
+        "area": "関東",
+        "prefName": "栃木県",
+        "shortName": "栃木",
+        "prefHiragana": "とちぎけん",
+        "shortHiragana": "とちぎ"
+    },
+    {
+        "id": "pref_10",
+        "area": "関東",
+        "prefName": "群馬県",
+        "shortName": "群馬",
+        "prefHiragana": "ぐんまけん",
+        "shortHiragana": "ぐんま"
+    },
+    {
+        "id": "pref_11",
+        "area": "関東",
+        "prefName": "埼玉県",
+        "shortName": "埼玉",
+        "prefHiragana": "さいたまけん",
+        "shortHiragana": "さいたま"
+    },
+    {
+        "id": "pref_12",
+        "area": "関東",
+        "prefName": "千葉県",
+        "shortName": "千葉",
+        "prefHiragana": "ちばけん",
+        "shortHiragana": "ちば"
+    },
+    {
+        "id": "pref_13",
+        "area": "関東",
+        "prefName": "東京都",
+        "shortName": "東京",
+        "prefHiragana": "とうきょうと",
+        "shortHiragana": "とうきょう"
+    },
+    {
+        "id": "pref_14",
+        "area": "関東",
+        "prefName": "神奈川県",
+        "shortName": "神奈川",
+        "prefHiragana": "かながわけん",
+        "shortHiragana": "かながわ"
+    },
+    {
+        "id": "pref_15",
+        "area": "中部",
+        "prefName": "新潟県",
+        "shortName": "新潟",
+        "prefHiragana": "にいがたけん",
+        "shortHiragana": "にいがた"
+    },
+    {
+        "id": "pref_16",
+        "area": "中部",
+        "prefName": "富山県",
+        "shortName": "富山",
+        "prefHiragana": "とやまけん",
+        "shortHiragana": "とやま"
+    },
+    {
+        "id": "pref_17",
+        "area": "中部",
+        "prefName": "石川県",
+        "shortName": "石川",
+        "prefHiragana": "いしかわけん",
+        "shortHiragana": "いしかわ"
+    },
+    {
+        "id": "pref_18",
+        "area": "中部",
+        "prefName": "福井県",
+        "shortName": "福井",
+        "prefHiragana": "ふくいけん",
+        "shortHiragana": "ふくい"
+    },
+    {
+        "id": "pref_19",
+        "area": "中部",
+        "prefName": "山梨県",
+        "shortName": "山梨",
+        "prefHiragana": "やまなしけん",
+        "shortHiragana": "やまなし"
+    },
+    {
+        "id": "pref_20",
+        "area": "中部",
+        "prefName": "長野県",
+        "shortName": "長野",
+        "prefHiragana": "ながのけん",
+        "shortHiragana": "ながの"
+    },
+    {
+        "id": "pref_21",
+        "area": "中部",
+        "prefName": "岐阜県",
+        "shortName": "岐阜",
+        "prefHiragana": "ぎふけん",
+        "shortHiragana": "ぎふ"
+    },
+    {
+        "id": "pref_22",
+        "area": "中部",
+        "prefName": "静岡県",
+        "shortName": "静岡",
+        "prefHiragana": "しずおかけん",
+        "shortHiragana": "しずおか"
+    },
+    {
+        "id": "pref_23",
+        "area": "中部",
+        "prefName": "愛知県",
+        "shortName": "愛知",
+        "prefHiragana": "あいちけん",
+        "shortHiragana": "あいち"
+    },
+    {
+        "id": "pref_24",
+        "area": "近畿",
+        "prefName": "三重県",
+        "shortName": "三重",
+        "prefHiragana": "みえけん",
+        "shortHiragana": "みえ"
+    },
+    {
+        "id": "pref_25",
+        "area": "近畿",
+        "prefName": "滋賀県",
+        "shortName": "滋賀",
+        "prefHiragana": "しがけん",
+        "shortHiragana": "しが"
+    },
+    {
+        "id": "pref_26",
+        "area": "近畿",
+        "prefName": "京都府",
+        "shortName": "京都",
+        "prefHiragana": "きょうとふ",
+        "shortHiragana": "きょうと"
+    },
+    {
+        "id": "pref_27",
+        "area": "近畿",
+        "prefName": "大阪府",
+        "shortName": "大阪",
+        "prefHiragana": "おおさかふ",
+        "shortHiragana": "おおさか"
+    },
+    {
+        "id": "pref_28",
+        "area": "近畿",
+        "prefName": "兵庫県",
+        "shortName": "兵庫",
+        "prefHiragana": "ひょうごけん",
+        "shortHiragana": "ひょうご"
+    },
+    {
+        "id": "pref_29",
+        "area": "近畿",
+        "prefName": "奈良県",
+        "shortName": "奈良",
+        "prefHiragana": "ならけん",
+        "shortHiragana": "なら"
+    },
+    {
+        "id": "pref_30",
+        "area": "近畿",
+        "prefName": "和歌山県",
+        "shortName": "和歌山",
+        "prefHiragana": "わかやまけん",
+        "shortHiragana": "わかやま"
+    },
+    {
+        "id": "pref_31",
+        "area": "中国",
+        "prefName": "鳥取県",
+        "shortName": "鳥取",
+        "prefHiragana": "とっとりけん",
+        "shortHiragana": "とっとり"
+    },
+    {
+        "id": "pref_32",
+        "area": "中国",
+        "prefName": "島根県",
+        "shortName": "島根",
+        "prefHiragana": "しまねけん",
+        "shortHiragana": "しまね"
+    },
+    {
+        "id": "pref_33",
+        "area": "中国",
+        "prefName": "岡山県",
+        "shortName": "岡山",
+        "prefHiragana": "おかやまけん",
+        "shortHiragana": "おかやま"
+    },
+    {
+        "id": "pref_34",
+        "area": "中国",
+        "prefName": "広島県",
+        "shortName": "広島",
+        "prefHiragana": "ひろしまけん",
+        "shortHiragana": "ひろしま"
+    },
+    {
+        "id": "pref_35",
+        "area": "中国",
+        "prefName": "山口県",
+        "shortName": "山口",
+        "prefHiragana": "やまぐちけん",
+        "shortHiragana": "やまぐち"
+    },
+    {
+        "id": "pref_36",
+        "area": "四国",
+        "prefName": "徳島県",
+        "shortName": "徳島",
+        "prefHiragana": "とくしまけん",
+        "shortHiragana": "とくしま"
+    },
+    {
+        "id": "pref_37",
+        "area": "四国",
+        "prefName": "香川県",
+        "shortName": "香川",
+        "prefHiragana": "かがわけん",
+        "shortHiragana": "かがわ"
+    },
+    {
+        "id": "pref_38",
+        "area": "四国",
+        "prefName": "愛媛県",
+        "shortName": "愛媛",
+        "prefHiragana": "えひめけん",
+        "shortHiragana": "えひめ"
+    },
+    {
+        "id": "pref_39",
+        "area": "四国",
+        "prefName": "高知県",
+        "shortName": "高知",
+        "prefHiragana": "こうちけん",
+        "shortHiragana": "こうち"
+    },
+    {
+        "id": "pref_40",
+        "area": "九州・沖縄",
+        "prefName": "福岡県",
+        "shortName": "福岡",
+        "prefHiragana": "ふくおかけん",
+        "shortHiragana": "ふくおか"
+    },
+    {
+        "id": "pref_41",
+        "area": "九州・沖縄",
+        "prefName": "佐賀県",
+        "shortName": "佐賀",
+        "prefHiragana": "さがけん",
+        "shortHiragana": "さが"
+    },
+    {
+        "id": "pref_42",
+        "area": "九州・沖縄",
+        "prefName": "長崎県",
+        "shortName": "長崎",
+        "prefHiragana": "ながさきけん",
+        "shortHiragana": "ながさき"
+    },
+    {
+        "id": "pref_43",
+        "area": "九州・沖縄",
+        "prefName": "熊本県",
+        "shortName": "熊本",
+        "prefHiragana": "くまもとけん",
+        "shortHiragana": "くまもと"
+    },
+    {
+        "id": "pref_44",
+        "area": "九州・沖縄",
+        "prefName": "大分県",
+        "shortName": "大分",
+        "prefHiragana": "おおいたけん",
+        "shortHiragana": "おおいた"
+    },
+    {
+        "id": "pref_45",
+        "area": "九州・沖縄",
+        "prefName": "宮崎県",
+        "shortName": "宮崎",
+        "prefHiragana": "みやざきけん",
+        "shortHiragana": "みやざき"
+    },
+    {
+        "id": "pref_46",
+        "area": "九州・沖縄",
+        "prefName": "鹿児島県",
+        "shortName": "鹿児島",
+        "prefHiragana": "かごしまけん",
+        "shortHiragana": "かごしま"
+    },
+    {
+        "id": "pref_47",
+        "area": "九州・沖縄",
+        "prefName": "沖縄県",
+        "shortName": "沖縄",
+        "prefHiragana": "おきなわけん",
+        "shortHiragana": "おきなわ"
+    }
+]
---
+++ script.js
@@ -0,0 +1,231 @@
+'use strict'
+
+// 地図のロック状態 => rxjs.jsファイルで使用
+let mapLocked;
+
+
+
+
+/**
+ * 引数のidと一致する都道府県の情報を返す
+ * @param {string} id - 都道府県を識別するためのid文字列 pref_xx
+ * @returns {object} - 一致した都道府県情報のオブジェクト
+ */
+const getPrefInfo = id => {
+    for (const pref of prefsInfo) {
+        if (pref["id"] === id) return pref;
+    }
+}
+
+/**
+ * 
+ */
+let mapLock = (locke) => {
+    let locked2 = locke;
+    const lock = () => {
+        return locked2;
+    };
+    mapLock.locked = () => locked2;
+    return lock;
+}
+mapLock(true);
+
+
+/**
+ * マップ表示のViewBox（座標と縮尺）を設定しマップに反映する
+ * 引数が無い場合はズームと位置を沖縄移動の設定に合わせ調整する
+ * @param {Array} viewParams - viewBoxを設定する配列又は空を受け取る [min-x min-y width height]
+ */
+const setViewBox = viewParam => {
+    const svg = document.querySelector("#svg_japan");
+    const okinawa = document.querySelector("#pref_47");
+    const okinawaLine = document.querySelector("#pref_line");
+    const tglOkinawa = document.querySelector("#okinawa_move");
+
+    if (Array.isArray(viewParam)) {
+        svg.setAttribute("viewBox", viewParam.join(" "));
+    } else if (tglOkinawa.checked) {
+        okinawa.setAttribute("transform", "translate(540,-250)");
+        okinawaLine.style.display = "block";
+        svg.setAttribute("viewBox", "-500 -20 1935 1637");
+    } else {
+        okinawa.setAttribute("transform", "translate(0,0)");
+        okinawaLine.style.display = "none";
+        svg.setAttribute("viewBox", "-755 -25 2380 2010");
+    }
+}
+
+/**
+ * 各アプリで表示している要素をすべて非表示にする
+ * 都道府県の要素のstyle.fillを初期化する
+ */
+const hideAppElements = () => {
+    document.querySelectorAll(".map-app").forEach((elm) => {
+        elm.style.display = "none";
+    });
+    document.querySelectorAll(".jp-pref").forEach((elm) => {
+        elm.style.fill = "";
+    });
+}
+
+/**
+ * マップ上の都道府県名の表示/非表示を切り替える
+ * @param {boolean} display - 表示させる場合はtrueを受け取とる
+ */
+const switchPrefName = display => {
+    document.querySelectorAll(".jp-pref-name").forEach((elm) => {
+        console.log(elm);
+        elm.style.display = display ? "block" : "none";
+    });    
+}
+
+// ランダムに返す
+const rndChoice = array => {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+/**
+ * 引数で受け取った要素に受け取った文字をタイピング風に出力
+ * @param {object} element - 文字列の出力先の要素
+ * @returns {object} - タイピングを開始する関数
+ */
+const typing = (element) => {
+    const defaultMsecDelay = 30;
+    let index = 0;
+    let intervalId;
+    let summary;
+    const write = () => {
+        element.innerHTML += summary[index];
+        index++;
+        if (index > summary.length - 1) {
+            clearInterval(intervalId);
+        }
+    }
+    const execInterval = (argSumm, argMsDelay) => {
+        execInterval.reset();
+        summary = argSumm;
+        const msDelay = argMsDelay > 0 ? argMsDelay : defaultMsecDelay;
+        intervalId = setInterval(write, msDelay);
+    }
+    execInterval.start = () => execInterval();
+    execInterval.stop = () => clearInterval(intervalId);
+    execInterval.reset = () => {
+        clearInterval(intervalId);
+        element.innerText = "";
+        index = 0;
+    }
+    return execInterval;
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // プレゼン発表用オープニング表示
+    if (window.location.search === "?presen") {
+        const head = document.querySelector("head");
+        const script = document.createElement("script");
+        script.src = "./lib/presenOpening.js";
+        head.appendChild(script);
+    }
+
+    const tglPrefNmae = document.querySelector("#disp_pref_name");
+    const tglOkinawa = document.querySelector("#okinawa_move");
+    const tglMapLock = document.querySelector("#map_lock");
+
+    // リサイズ対応（※実装中...）
+    // window.addEventListener("resize", () => {
+    //     // console.log(window.innerWidth , window.innerHeight );
+    // });
+
+    // メニュー
+    const knowit = prefecturesKnowit(); // prefecturesKnowit.jsにて定義
+    const quiz = prefecturesQuiz(); // prefecturesQuiz.jsにて定義
+    const heatMap = prefecturesHeatMap();   // prefecturesHeatMap.jsにて定義
+    document.querySelectorAll(".menu-btn").forEach( elm => {
+        elm.addEventListener("click", (e) => {
+
+            // TOP
+            if (elm.id === "menu_top") {
+                hideAppElements();
+                if (tglOkinawa.checked) {
+                    tglOkinawa.click();
+                }
+                if (tglMapLock.checked) {
+                    tglMapLock.click();
+                }
+                setViewBox();
+            }
+
+            // 都道府県を知ろう
+            if (elm.id === "manu_pref_knowit") {
+                hideAppElements();
+                knowit();
+            }
+            
+            // 都道府県当てクイズ
+            if (elm.id === "menu_pref_quiz") {
+                hideAppElements();
+                quiz();
+            }
+            
+            // おまけ
+            if (elm.id === "menu_bonus") {
+                hideAppElements();
+                heatMap();
+            }
+
+        });
+    });
+    
+    // オプション設定
+    document.querySelectorAll(".cnf-tgl > input").forEach( elm => {
+        elm.addEventListener("change", (e) => {
+            
+            // 県名表示(※挫折中 一部のみ)
+            if (elm.id === "disp_pref_name") {
+                switchPrefName(tglPrefNmae.checked);
+            }
+        
+            // 沖縄移動
+            if (elm.id === "okinawa_move") {
+                setViewBox();
+            }
+            
+            // 地図ロック(※./lib/rxjs.jsにて変数使用)
+            if (elm.id === "map_lock") {
+                mapLocked = elm.checked;
+            }
+
+            // 知名度UP
+            if (elm.id === "disp_pref_highlite") {
+                document.querySelector("#pref_24").style.fill = "#ff11c7";
+            }
+            
+        });
+    });
+    
+    // 各県クリックイベント（※debug or 将来用）
+    document.querySelectorAll(".jp-pref").forEach( elm => {
+        if (elm.id === "pref_line") {
+            return;
+        }
+        elm.addEventListener("click", (e) => {
+            // console.log(getPrefInfo(elm.id).prefName);
+
+
+            elm.style.fill = "#94EB85";
+            
+
+
+
+        });
+    });
+
+});
+
+// ページロード完了
+window.onload = function() {
+    // console.log("window loaded");
+};
+
+
